aca9fb496112450d413bd871a41c243e3170583d
Omer Kirnap <okirnap@ku.edu.tr>
2017-10-19 16:06:56 +0300

    add usefulconstants

---
+++ params_lstm.jl
@@ -0,0 +1,11 @@
+const EPOCHS=3
+const BATCHSIZE=64
+const EMBEDSIZE=125
+const HUMHIDDEN=100
+const DROPOUT=0.2
+const LR=0.001
+const BETA_1=0.9
+const BETA_2=0.999
+const EPS=1e-08
+const MAXLEN=150 #maximum size of the word sequence
+const MAXFEATURES=30000 #vocabulary size

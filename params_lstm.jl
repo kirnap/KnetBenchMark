216c616e13e09428a6790205e4c907d9b17b1bca
Omer Kirnap <okirnap@ku.edu.tr>
2017-10-20 20:17:01 +0300

    modify preprocess

--- params_lstm.jl
+++ params_lstm.jl
@@ -1,7 +1,7 @@
 const EPOCHS=3
 const BATCHSIZE=64
 const EMBEDSIZE=125
-const HUMHIDDEN=100
+const NUMHIDDEN=100
 const DROPOUT=0.2
 const LR=0.001
 const BETA_1=0.9
@@ -9,3 +9,4 @@ const BETA_2=0.999
 const EPS=1e-08
 const MAXLEN=150 #maximum size of the word sequence
 const MAXFEATURES=30000 #vocabulary size
+const outlabel=2 #output label in sentiment analysis
--- preprocess.jl
+++ preprocess.jl
@@ -2,11 +2,24 @@
 
 # To preprocess the pre-loaded data
 # The reason why we preloaded the data is it was saved txt files
-function imdb_for_library(;datadir="data/RNN")
-    f(x) = map(x->convert(Int, x), readdlm("$datadir/$x"))
-    x_train = f("x_train.txt")
-    x_test = f("x_test.txt")
-    y_train = f("y_train.txt")
-    y_test = f("y_test.txt")
-    return (x_train, x_test, y_train, y_test)
+let
+    f(x::AbstractString, y::AbstractString) = map(x->convert(Int, x), readdlm("$y/$x"))
+    nz(t::Array{Int64, 1}) = sum(t .== 0)
+    function correct_data(data::Array)
+        x_correct = fill!(similar(data), 0)
+        for i in 1:size(x_train)[1]
+            nzeros = nz(x_train[i,:])
+            x_correct[i, nzeros+1:end] = x_train[i, 1:end-nzeros]
+        end
+        return x_correct
+    end
+    global imdb_for_library
+    function imdb_for_library(;datadir="data/RNN")
+        x_train = correct_data(f("x_train.txt", datadir))
+        x_test = correct_data(f("x_test.txt", datadir))
+        y_train = f("y_train.txt", datadir)
+        y_test = f("y_test.txt", datadir)
+        # Here we need to manipulate the x_train matrix
+        return (x_train, x_test, y_train, y_test)
+    end
 end

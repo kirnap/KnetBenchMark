21d8eb2a4a7da8514093cf3e821e20d294f216a1
Omer Kirnap <okirnap@ku.edu.tr>
2017-10-21 14:57:29 +0300

    add minibatching

--- preprocess.jl
+++ preprocess.jl
@@ -2,6 +2,7 @@
 
 # To preprocess the pre-loaded data
 # The reason why we preloaded the data is it was saved txt files
+# x train matrix needs to be manipulated before-hand
 let
     f(x::AbstractString, y::AbstractString) = map(x->convert(Int, x), readdlm("$y/$x"))
     nz(t::Array{Int64, 1}) = sum(t .== 0)
@@ -16,10 +17,35 @@ let
     global imdb_for_library
     function imdb_for_library(;datadir="data/RNN")
         x_train = correct_data(f("x_train.txt", datadir))
-        x_test = correct_data(f("x_test.txt", datadir))
         y_train = f("y_train.txt", datadir)
+        info("Train data loaded")
+        info("Test data loaded")
+        x_test = correct_data(f("x_test.txt", datadir))
         y_test = f("y_test.txt", datadir)
-        # Here we need to manipulate the x_train matrix
         return (x_train, x_test, y_train, y_test)
+    end
+end
+
+# Minibatch 1 assumes correct minibatching mechanism for sequence labeling
+let f(x::Int) = (x==0 ? 2 : x); global minibatch
+    function minibatch1(corpus, batchsize)
+        data  = Any[]
+        masks = Any[]
+        for i in 1:batchsize:size(corpus)[1]
+            j = min(i+batchsize-1, size(corpus)[1])
+            batch = corpus[i:j, :]
+            sequences = Any[]
+            _mask = Any[]
+            for r in 1:size(batch)[2]
+                mask = ones(Float32, 1, (j-i+1))
+                mask[find(t->t==0, batch[:, r])] = 0.0
+                #x = (batch[:,r], mask)
+                push!(sequences, batch[:, r])
+                push!(_mask, mask)
+            end
+            push!(data, sequences)
+            push!(masks, _mask)
+        end
+        return data, masks
     end
 end
